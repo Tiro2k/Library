31ead102266433b0ada1ee32676bff0d9b5d22e3
yjiang <jeremy_jiangyi@hotmail.com>
2024-12-19 17:11:48 +0800

    初始化Allegro器件

---
+++ Cadence/Allegro/allegroload.bat
@@ -0,0 +1,6 @@
+if exist r160_130.pad del r160_130.pad
+padstack_editor -s R160_130.SCR
+if exist c135h90.pad del c135h90.pad
+padstack_editor -s C135H90.SCR
+if exist SW10X10-6P.dra del SW10X10-6P.dra
+allegro -s SW10X10-6P.SCR
