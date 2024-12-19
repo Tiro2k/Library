480a5edd04a83d59593cb99057517b6abde3a156
yijiang <jeremy_jiangyi@hotmail.com>
2024-12-20 06:37:23 +0800

    增加MAX7219/8X8点阵/6键开关

--- Cadence/Allegro/allegroload.bat
+++ Cadence/Allegro/allegroload.bat
@@ -1,4 +1,24 @@
-if exist r104_60r15.pad del r104_60r15.pad
-padstack_editor -s R104_60R15.SCR
-if exist SOT23-BJTN.dra del SOT23-BJTN.dra
-allegro -s SOT23-BJTN.SCR
+if exist r160_130.pad del r160_130.pad
+padstack_editor -s R160_130.SCR
+if exist c135h90.pad del c135h90.pad
+padstack_editor -s C135H90.SCR
+if exist r200_180.pad del r200_180.pad
+padstack_editor -s R200_180.SCR
+if exist s200.pad del s200.pad
+padstack_editor -s S200.SCR
+if exist r190_64r16.pad del r190_64r16.pad
+padstack_editor -s R190_64R16.SCR
+if exist s121h80.pad del s121h80.pad
+padstack_editor -s S121H80.SCR
+if exist c121h80.pad del c121h80.pad
+padstack_editor -s C121H80.SCR
+if exist SW10X10-6P.dra del SW10X10-6P.dra
+allegro -s SW10X10-6P.SCR
+if exist SW12X12.dra del SW12X12.dra
+allegro -s SW12X12.SCR
+if exist LEDSEG4.dra del LEDSEG4.dra
+allegro -s LEDSEG4.SCR
+if exist MAX7219N.dra del MAX7219N.dra
+allegro -s MAX7219N.SCR
+if exist DMG8X8.dra del DMG8X8.dra
+allegro -s DMG8X8.SCR
